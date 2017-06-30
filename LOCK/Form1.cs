ea28a03cafb54e1f387e02e5cda480821cf24e6d
Knove <308346293@qq.com>
2017-06-30 20:02:58 +0800

    FIX BUG

--- LOCK/Form1.cs
+++ LOCK/Form1.cs
@@ -89,10 +89,12 @@ namespace LOCK
                 this.listBox2.Items.Add(NextFile.Name);
 
                 //限定遍历的文件类型
-                if (NextFile.Name.Substring(NextFile.Name.Length - 3) == "txt" || 
-                            NextFile.Name.Substring(NextFile.Name.Length - 3) == "doc" ||
-                            NextFile.Name.Substring(NextFile.Name.Length - 4) == "docx")
-                    this.listBox3.Items.Add(@getmulu + "/" + NextFile.Name);
+                if (NextFile.Name.Substring(NextFile.Name.Length - 3) == "txt"
+
+                            //  || NextFile.Name.Substring(NextFile.Name.Length - 3) == "doc" ||
+                            // NextFile.Name.Substring(NextFile.Name.Length - 4) == "docx"
+                            )
+                        this.listBox3.Items.Add(@getmulu + "/" + NextFile.Name);
             }
 
 
@@ -109,9 +111,9 @@ namespace LOCK
                         this.listBox2.Items.Add(NextFile.Name);
 
                         //限定遍历的文件类型
-                       if( NextFile.Name.Substring( NextFile.Name.Length - 3)=="txt"|| 
-                            NextFile.Name.Substring(NextFile.Name.Length - 3) == "doc" ||
-                            NextFile.Name.Substring(NextFile.Name.Length - 4) == "docx"
+                       if( NextFile.Name.Substring( NextFile.Name.Length - 3)=="txt"
+                               // || NextFile.Name.Substring(NextFile.Name.Length - 3) == "doc" ||
+                            //NextFile.Name.Substring(NextFile.Name.Length - 4) == "docx"
                             )
                         this.listBox3.Items.Add(@getmulu + "/" + NextFolder.Name + "/" + NextFile.Name);
                     }
